a93684572495d1dc6d691c338e585c4364c0220b
Nikita <127978648+sweet-peach@users.noreply.github.com>
2025-05-30 05:36:09 +0200

    Cleanup

--- src/instrumentation-node.js
+++ src/instrumentation-node.js
@@ -4,23 +4,21 @@ import Database from "./server/database/database.js";
 // IMPORT HERE ALL MODELS SO SEQUELIZE COULD PROCESS THEM
 import './server/modification/ModificationModel.js';
 import './server/timeLog/TimeLogModel.js';
-import {getUserRoles} from "./server/authorization/AuthorizationService.js";
 
 const database = Database.getInstance();
 
 try {
-    // console.debug('Connecting to database');
-    // await database.connect();
-    // console.debug('Connected');
-    //
-    // console.debug('Defining associations');
-    // await defineAssociations();
-    // console.debug('Associations defined');
-    //
-    // console.debug('Synchronizing models with database');
-    // await database.sync();
-    // console.debug('Synchronized');
-    await getUserRoles('admin@mail.com')
+    console.debug('Connecting to database');
+    await database.connect();
+    console.debug('Connected');
+
+    console.debug('Defining associations');
+    await defineAssociations();
+    console.debug('Associations defined');
+
+    console.debug('Synchronizing models with database');
+    await database.sync();
+    console.debug('Synchronized');
 } catch (error) {
     console.error('Error in database connection or initialization');
     console.error(error);
