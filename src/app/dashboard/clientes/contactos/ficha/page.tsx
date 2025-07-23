77f8262b0c15a9905837bca85b196e8dc738ac0c
frank gimeno <frankgimeno3@gmail.com>
2025-07-23 14:12:54 +0200

    creo nuevas páginas de ficha de cliente y contacto

---
+++ src/app/dashboard/clientes/contactos/ficha/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface FichaContactoProps {
+  
+}
+
+const FichaContacto: FC<FichaContactoProps> = ({ }) => {
+  return (
+    <div>FichaContacto</div>
+  );
+};
+
+export default FichaContacto;
---
+++ src/app/dashboard/clientes/ficha/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface FichaClienteProps {
+  
+}
+
+const FichaCliente: FC<FichaClienteProps> = ({ }) => {
+  return (
+    <div>FichaCliente</div>
+  );
+};
+
+export default FichaCliente;
