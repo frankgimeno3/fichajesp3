0bf2e0d4029879a1a98b9716a3c9c6e8054dc7c3
frank gimeno <frankgimeno3@gmail.com>
2025-07-26 03:31:03 +0200

    creo primer contenido de ficha, y opcion de editar

---
+++ src/app/dashboard/clientes/ficha/editar/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface EditarFichaProps {
+  
+}
+
+const EditarFicha: FC<EditarFichaProps> = ({ }) => {
+  return (
+    <div>EditarFicha</div>
+  );
+};
+
+export default EditarFicha;
--- src/app/dashboard/clientes/ficha/page.tsx
+++ src/app/dashboard/clientes/ficha/page.tsx
@@ -1,12 +1,53 @@
-import React, { FC } from 'react';
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { FC, useState } from 'react';
 
-interface FichaClienteProps {
-  
-}
+interface FichaClienteProps {}
 
 const FichaCliente: FC<FichaClienteProps> = ({ }) => {
+  const router = useRouter();
+  const [pestana, setPestana] = useState('general');
+
+  const tabs = [
+    { id: 'general', label: 'General' },
+    { id: 'administrativo', label: 'Administrativo' },
+    { id: 'registro', label: 'Registro' },
+  ];
+
   return (
-    <div>FichaCliente</div>
+    <div className="bg-gray-100 h-full min-h-screen p-12 text-gray-600">
+      <div className='flex flex-row justify-between w-full items-center'>
+        <h2 className="text-lg font-semibold mb-4">
+          Ficha de cliente   
+          <span className='px-6 font-light'>Código: 01234</span> 
+          <span className='px-6 font-light'>Nombre: Turomas</span>
+        </h2>
+        <button 
+          className='bg-blue-950 text-gray-100 p-2 px-4 rounded-lg shadow-xl cursor-pointer hover:bg-blue-900'
+          onClick={() => router.push('/dashboard/Clientes/ficha/editar')}
+        >
+          <p>Editar</p>
+        </button>
+      </div>
+      
+      <div className='flex flex-row mt-12 gap-4'>
+        {tabs.map(tab => (
+          <p
+            key={tab.id}
+            className={`p-3 rounded-lg shadow-xl cursor-pointer hover:bg-white/70 ${
+              pestana === tab.id ? 'bg-white' : 'bg-gray-200/80'
+            }`}
+            onClick={() => setPestana(tab.id)}
+          >
+            {tab.label}
+          </p>
+        ))}
+      </div>
+
+      <div className='bg-white p-12 mt-6'>
+        <p>Nombre de la empresa</p>
+      </div>
+    </div>
   );
 };
 
