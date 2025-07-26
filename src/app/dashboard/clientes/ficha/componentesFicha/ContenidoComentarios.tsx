b35728d3c5633c6f537c83fd11812664ade8a85f
frank gimeno <frankgimeno3@gmail.com>
2025-07-26 04:18:38 +0200

    completo estructura comentarios

--- src/app/dashboard/clientes/ficha/componentesFicha/ContenidoComentarios.tsx
+++ src/app/dashboard/clientes/ficha/componentesFicha/ContenidoComentarios.tsx
@@ -2,17 +2,31 @@ import React, { FC } from 'react';
 import CardComentario from './CardComentario';
 
 interface ContenidoComentariosProps {
-  
+
 }
 
 const ContenidoComentarios: FC<ContenidoComentariosProps> = ({ }) => {
-  return (
-    <div>
-        <p>Comentarios</p>
-        <p>Apartado para buscar comentarios</p>
-        <CardComentario/>
-    </div>
-  );
+    return (
+        <div className='flex flex-col'>
+            <div className='text-right w-full '>
+                <button className='bg-blue-950 text-gray-100 p-2 px-4 rounded-lg shadow-xl cursor-pointer hover:bg-blue-950/90' >
+                    Añadir comentario
+                </button>
+
+            </div>
+            <div className='flex flex-col py-5 gap-3'>
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+                <CardComentario />
+            </div>
+        </div>
+    );
 };
 
 export default ContenidoComentarios;
