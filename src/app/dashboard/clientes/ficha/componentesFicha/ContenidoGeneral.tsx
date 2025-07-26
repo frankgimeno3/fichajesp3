3e96d76722488dac779f38cb7130690ac4fb5a94
frank gimeno <frankgimeno3@gmail.com>
2025-07-26 04:24:24 +0200

    brainstorming contenido general ficha

--- src/app/dashboard/clientes/ficha/componentesFicha/ContenidoGeneral.tsx
+++ src/app/dashboard/clientes/ficha/componentesFicha/ContenidoGeneral.tsx
@@ -6,7 +6,28 @@ interface ContenidoGeneralProps {
 
 const ContenidoGeneral: FC<ContenidoGeneralProps> = ({ }) => {
   return (
-    <div>ContenidoGeneral</div>
+    <div className='flex flex-col'>
+      <div className='py-12'>
+        <p>Nombre comercial de la empresa: <span className='pl-5'>Valor</span></p> 
+        <p>País: <span className='pl-5'>Valor</span></p> 
+        <p>Ciudad: <span className='pl-5'>Valor</span></p> 
+        <p>Código postal: <span className='pl-5'>Valor</span></p> 
+        <p>Teléfono principal (con prefijo): <span className='pl-5'>Valor</span></p> 
+                <p>Sector: <span className='pl-5'>Valor</span></p> 
+        <p>Código postal: <span className='pl-5'>Valor</span></p> 
+
+      </div>
+
+      <div className='py-12'>
+        <p>Ferias asistidas: <span className='pl-5'>Valor</span></p> 
+        <p>Distribuidores: <span className='pl-5'>Valor</span></p> 
+        <p>Agencia: <span className='pl-5'>Valor</span></p> 
+        <p>Grupo: <span className='pl-5'>Valor</span></p> 
+        <p>Filiales: <span className='pl-5'>Valor</span></p> 
+        <p>Categoría comercial: <span className='pl-5'>Valor</span></p> 
+      </div>
+
+    </div>
   );
 };
 
