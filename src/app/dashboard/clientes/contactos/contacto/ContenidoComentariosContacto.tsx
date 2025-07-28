19ae360b6ea48107d4b7e7e825fd06bb46749831
frank gimeno <frankgimeno3@gmail.com>
2025-07-28 12:04:52 +0200

    creo esqueleto contactos

---
+++ src/app/dashboard/clientes/contactos/contacto/ContenidoComentariosContacto.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface ContenidoComentariosContactoProps {
+  
+}
+
+const ContenidoComentariosContacto: FC<ContenidoComentariosContactoProps> = ({ }) => {
+  return (
+    <div>ContenidoComentariosContacto</div>
+  );
+};
+
+export default ContenidoComentariosContacto;
---
+++ src/app/dashboard/clientes/contactos/contacto/ContenidoGeneralContacto.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface ContenidoGeneralContactoProps {
+  
+}
+
+const ContenidoGeneralContacto: FC<ContenidoGeneralContactoProps> = ({ }) => {
+  return (
+    <div>ContenidoGeneralContacto</div>
+  );
+};
+
+export default ContenidoGeneralContacto;
