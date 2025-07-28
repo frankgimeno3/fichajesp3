a51b457c3e4f633122c064f21071a4315ddd0d12
frank gimeno <frankgimeno3@gmail.com>
2025-07-28 22:54:14 +0200

    se muestra contenido por cliente

--- src/app/dashboard/seguimientos/campanas/porcliente/ClientesCards.tsx
+++ src/app/dashboard/seguimientos/campanas/porcliente/ClientesCards.tsx
@@ -6,19 +6,21 @@ interface ClientesCardsProps {
 
 const ClientesCards: FC<ClientesCardsProps> = ({ }) => {
     return (
-        <div className='flex flex-col bg-gray-100/50 border border-gray-100 p-12'>
-            <p>Turomas</p>
-            <div className='flex flex-row'>
-                <p>Fecha de firma:</p>
-                <p className='pl-5'>12 / 12 / 2025</p>
+        <div className='flex flex-col bg-gray-100/60 hover:bg-gray-100/40 cursor-pointer border border-gray-100 px-6 py-3'>
+            <p className='font-bold text-gray-600'>Turomas</p>
+            <div className='flex flex-row gap-6'>
+            <div className='flex flex-row text-sm'>
+                <p className='font-bold text-gray-500'>Fecha de firma:</p>
+                <p className='pl-3'>12 / 12 / 2025</p>
             </div>
-            <div className='flex flex-row'>
-                <p>Fecha próxima deadline:</p>
-                <p className='pl-5'>10 / 11 / 2025</p>
+            <div className='flex flex-row text-sm'>
+                <p className='font-bold text-gray-500'>Fecha próxima deadline:</p>
+                <p className='pl-3'>10 / 11 / 2025</p>
             </div>
-                        <div className='flex flex-row'>
-                <p>Persona de contacto:</p>
-                <p className='pl-5'>Miguel rodriguez5</p>
+                        <div className='flex flex-row text-sm'>
+                <p className='font-bold text-gray-500'>Persona de contacto:</p>
+                <p className='pl-3'>Miguel rodriguez5</p>
+            </div>
             </div>
         </div>
     );
--- src/app/dashboard/seguimientos/campanas/porcliente/ContenidoPorCliente.tsx
+++ src/app/dashboard/seguimientos/campanas/porcliente/ContenidoPorCliente.tsx
@@ -1,12 +1,33 @@
+"use client"
 import React, { FC } from 'react';
+import ClientesCards from './ClientesCards';
+import { useRouter } from 'next/navigation';
 
 interface ContenidoPorClienteProps {
   
 }
 
 const ContenidoPorCliente: FC<ContenidoPorClienteProps> = ({ }) => {
+    const router = useRouter()
+  
   return (
-    <div>ContenidoPorCliente</div>
+    <div className='flex flex-col'>
+      <div className='flex flex-row mb-6 border border-gray-200 text-gray-100 rounded-sm' style={{"width":"600px"}}>
+        <input className='p-2 px-5 placeholder-gray-400 w-full' placeholder='Buscar por empresa' />
+        <button
+          className='bg-blue-950 text-gray-100 p-2 px-4 rounded-lg shadow-xl cursor-pointer hover:bg-blue-900' 
+          onClick={() => router.push('/dashboard/seguimientos/campanas')}
+        >
+          <p>Buscar</p>
+        </button>
+      </div>
+    <div className='flex flex-col gap-3'>
+      <ClientesCards/>
+      <ClientesCards/>
+      <ClientesCards/>
+      <ClientesCards/>
+    </div>
+    </div>
   );
 };
 
