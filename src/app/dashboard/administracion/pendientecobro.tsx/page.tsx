52465fe7bfd1453443fa4cafdcc0236f244bed24
frank gimeno <frankgimeno3@gmail.com>
2025-07-30 14:40:15 +0200

    creo pagina pendiente de cobro

---
+++ src/app/dashboard/administracion/pendientecobro.tsx/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface PendienteCobroProps {
+  
+}
+
+const PendienteCobro: FC<PendienteCobroProps> = ({ }) => {
+  return (
+    <div>PendienteCobro</div>
+  );
+};
+
+export default PendienteCobro;
