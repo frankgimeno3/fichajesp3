8af500ba79148abdfe9fedabdf0b678d14202aaf
frank gimeno <frankgimeno3@gmail.com>
2025-08-02 16:58:12 +0200

    agrego midnav a campañas administrativas

--- src/app/dashboard/administracion/campanas/page.tsx
+++ src/app/dashboard/administracion/campanas/page.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import MiddleNav from '../../0dashboardcomponents/MiddleNav';
 
 interface CampanasAdministrativasProps {}
 
@@ -17,7 +18,10 @@ const CampanasAdministrativas: FC<CampanasAdministrativasProps> = () => {
   ];
 
   return (
-    <div className="p-4">
+    <div className="flex flex-col  h-full min-h-screen text-gray-600">
+
+      <MiddleNav tituloprincipal={` Campañas administrativas    `} />
+      <div className="bg-gray-200 min-h-screen p-12 text-gray-600">
       <h2 className="text-xl font-bold mb-4">Campañas Administrativas</h2>
       <div className="overflow-x-auto">
         <table className="min-w-full table-auto border border-gray-300">
@@ -42,6 +46,7 @@ const CampanasAdministrativas: FC<CampanasAdministrativasProps> = () => {
             </tr>
           </tbody>
         </table>
+      </div>
       </div>
     </div>
   );
