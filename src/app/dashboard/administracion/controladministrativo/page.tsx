1120dd80eff354f1ffbb6d780f5520763ae45e82
frank gimeno <frankgimeno3@gmail.com>
2025-08-02 19:03:42 +0200

    configuro el midnav en controladministrativo

--- src/app/dashboard/administracion/controladministrativo/page.tsx
+++ src/app/dashboard/administracion/controladministrativo/page.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from 'react';
+import MiddleNav from '../../0dashboardcomponents/MiddleNav';
 
 interface ControlAdministrativoProps {
-  
+
 }
 
 const ControlAdministrativo: FC<ControlAdministrativoProps> = ({ }) => {
   return (
-    <div>ControlAdministrativo</div>
+
+    <div className="flex flex-col bg-gray-200 h-full min-h-screen text-gray-600">
+
+      <MiddleNav tituloprincipal={` Control administrativo  `} />
+
+      <div className="bg-white min-h-screen p-12 text-gray-600">
+      </div>
+    </div>
   );
 };
 
