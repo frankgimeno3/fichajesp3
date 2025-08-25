c4d8b64c7c2e681809f4b57d3892228a9b4dd2bd
frank gimeno <frankgimeno3@gmail.com>
2025-08-25 14:16:13 +0200

    renderizado condicional fases

--- src/app/dashboard/cuentas/contactos/crear/page.tsx
+++ src/app/dashboard/cuentas/contactos/crear/page.tsx
@@ -1,4 +1,7 @@
 import React, { FC, useState } from 'react';
+import Fase1 from './fases/fase1';
+import Fase2 from './fases/fase2';
+import Fase3 from './fases/fase3';
 
 interface CrearContactoProps {
   
@@ -7,7 +10,11 @@ interface CrearContactoProps {
 const CrearContacto: FC<CrearContactoProps> = ({ }) => {
   const [faseCrearContacto, setFaseCrearContacto] = useState(0)
   return (
-    <div>CrearContacto</div>
+    <div>
+      {faseCrearContacto == 0 && <Fase1/>}
+      {faseCrearContacto == 1 && <Fase2/>}
+      {faseCrearContacto == 2 && <Fase3/>}
+    </div>
   );
 };
 
