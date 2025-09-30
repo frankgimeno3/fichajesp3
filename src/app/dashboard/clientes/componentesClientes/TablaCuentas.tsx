da231780175f51d7b55b35f26a573ca487c30b2c
frank gimeno <frankgimeno3@gmail.com>
2025-10-01 00:45:41 +0200

    modifico redireccion desde tabla cuentas

--- src/app/dashboard/clientes/componentesClientes/TablaCuentas.tsx
+++ src/app/dashboard/clientes/componentesClientes/TablaCuentas.tsx
@@ -73,7 +73,7 @@ const Tablacuentas: FC<TablacuentasProps> = ({
             <tr
               key={res.id}
               className="border-t border-gray-200 hover:bg-gray-100/30 cursor-pointer"
-              onClick={() => handleRedirection('/dashboard/cuentas/ficha')}
+              onClick={() => handleRedirection('/dashboard/clientes/cuentas/ficha')}
             >
                <td className='p-2 border-b border-gray-200'>{res.nombreEmpresa}</td>
                <td className='p-2 border-b border-gray-200'>{res.codigoCRM}</td>
