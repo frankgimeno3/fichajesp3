63b085114b18422e76ec07b079de00030687cda8
frank gimeno <frankgimeno3@gmail.com>
2025-11-24 17:39:10 +0100

    datos generales y de impresa renderizan correctamente en contrato

--- src/app/dashboard/comercial/contratos/[id_contrato]/resumencomponents/TablaDatosEmpresa.tsx
+++ src/app/dashboard/comercial/contratos/[id_contrato]/resumencomponents/TablaDatosEmpresa.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { InterfazContrato } from "@/app/interfaces/interfaces";
+import { InterfazContacto, InterfazContrato, InterfazCuenta } from "@/app/interfaces/interfaces";
+import cuentas from "@/app/contents/cuentasContents.json"
+import contactos from "@/app/contents/contactsContents.json"
 
 interface TablaDatosEmpresaProps {
   contrato: InterfazContrato;
@@ -10,6 +12,32 @@ const TablaDatosEmpresa: FC<TablaDatosEmpresaProps> = ({ contrato }) => {
   const router = useRouter();
   const { cuenta_contrato } = contrato;
 
+  const [cuentaSeleccionada, setCuentaSeleccionada] = useState<InterfazCuenta | undefined>(() =>
+    cuentas.find(
+      (cu) => cu.id_cuenta === contrato.cuenta_contrato.id_cuenta_contrato    )
+  ); 
+
+  useEffect(() => {
+  const cuenta = cuentas.find(
+    (cu) => cu.id_cuenta === contrato.cuenta_contrato.id_cuenta_contrato
+  );
+  setCuentaSeleccionada(cuenta);
+}, [contrato]);
+
+    const [contactoSeleccionado, setContactoSeleccionado] = useState<InterfazContacto | undefined>(() =>
+    contactos.find(
+      (co) => co.id_contacto === contrato.cuenta_contrato.id_contacto
+    )
+  );
+
+
+useEffect(() => {
+  const contacto = contactos.find(
+    (co) => co.id_contacto === contrato.cuenta_contrato.id_contacto
+  );
+  setContactoSeleccionado(contacto);
+}, [contrato]);
+
   return (
     <div className="overflow-x-auto">
       <table className="table-auto border-collapse w-full text-center">
@@ -17,7 +45,7 @@ const TablaDatosEmpresa: FC<TablaDatosEmpresaProps> = ({ contrato }) => {
           <tr className="bg-blue-950 text-white">
             <th className="px-6 py-2">Empresa anunciante</th>
             <th className="px-4 py-2">Código CRM</th>
-             <th className="px-4 py-2">País</th>
+            <th className="px-4 py-2">País</th>
             <th className="px-4 py-2">Contacto de gestión</th>
             <th className="px-4 py-2">Cargo del contacto</th>
           </tr>
@@ -27,13 +55,13 @@ const TablaDatosEmpresa: FC<TablaDatosEmpresaProps> = ({ contrato }) => {
             <td className="px-4 py-2">
               <button
                 className="bg-blue-950 text-gray-100 p-2 px-4 rounded-lg shadow-xl cursor-pointer hover:bg-blue-900"
-                onClick={() => router.push("/dashboard/clientes/ficha")}
+                onClick={() => router.push(`/dashboard/clientes/cuentas/${cuentaSeleccionada?.id_cuenta}`)}
               >
-                Empresa {cuenta_contrato.id_cuenta_contrato}
+                {cuentaSeleccionada?.nombre_empresa}
               </button>
             </td>
             <td className="px-4 py-2">{cuenta_contrato.id_cuenta_contrato}</td>
-            <td className="px-4 py-2">N/A</td>
+            <td className="px-4 py-2">{cuentaSeleccionada?.pais_cuenta}</td>
             <td className="px-4 py-2">
               <button
                 className="bg-blue-950 text-gray-100 p-2 px-4 rounded-lg shadow-xl cursor-pointer hover:bg-blue-900"
@@ -41,7 +69,7 @@ const TablaDatosEmpresa: FC<TablaDatosEmpresaProps> = ({ contrato }) => {
                   router.push(`/dashboard/clientes/contactos/${cuenta_contrato.id_contacto}`)
                 }
               >
-                {cuenta_contrato.id_contacto}
+                {contactoSeleccionado?.nombre_completo_contacto}
               </button>
             </td>
             <td className="px-4 py-2">{cuenta_contrato.cargoContacto}</td>
